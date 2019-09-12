ca7b979b7c84357b140f955cda9464b218a274ef
Alec-Shay <Alec.Shay@liferay.com>
2019-09-12 11:22:57 -0700

    Add language config

--- conf.py
+++ conf.py
@@ -71,7 +71,7 @@ release = '2.0.5'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = ['en', 'ja']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -172,7 +172,7 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-locale_dirs = ['locale/']
+locale_dirs = ['locales/']
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
